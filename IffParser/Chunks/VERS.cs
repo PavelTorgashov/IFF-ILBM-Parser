c7e3a03bb064727dd104aa400fa9163bccc2d3c1
Pavel Torgashov <tp_soft@mail.ru>
2016-10-04 10:08:39 +0300

    First release

---
+++ IffParser/Chunks/VERS.cs
@@ -0,0 +1,11 @@
+﻿namespace IffParserNS.Chunks
+{
+    public class VERS
+    {
+        public uint V0;
+        public uint V1;
+        public uint V2;
+        public uint V3;
+        public string Desc;
+    }
+}
---
+++ Tester/Program.cs
@@ -0,0 +1,54 @@
+﻿// (c) Pavel Torgashov, 2016.
+// GPL3
+
+using System;
+using System.IO;
+using IffParserNS;
+
+namespace Tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //------------------------------------
+            // Extracts bitmap from IFF ILBM file
+            //------------------------------------
+            var file = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "test.iff");
+            var outputFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "test.png");
+
+            try
+            {
+                //create IFF parser
+                var iff = new IFF();
+                //parse file
+                iff.Parse(file);
+
+                //check Bitmap
+                if (iff.Bitmap != null)
+                {
+                    iff.Bitmap.Save(outputFile);
+                }
+
+                //check JPEG chunk
+                if (iff.JPEG != null)
+                {
+                    var path = Path.GetDirectoryName(outputFile);
+                    var fileName = Path.GetFileNameWithoutExtension(outputFile) + "_preview.jpg";
+                    File.WriteAllBytes(Path.Combine(path, fileName), iff.JPEG);
+                }
+
+                if (iff.JPEG == null && iff.Bitmap == null)
+                    throw new Exception("There are no Images.");
+
+                Console.WriteLine("Conversion is successfull.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
